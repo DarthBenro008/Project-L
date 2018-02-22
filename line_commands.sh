c316c697e50e332a441d1f3009fbee372dd9f7a4
Hemanth Krishna <31009634+DarthBenro008@users.noreply.github.com>
2018-02-22 16:09:44 +0530

    Update line_commands.sh

--- line_commands.sh
+++ line_commands.sh
@@ -3,6 +3,6 @@ mkdir line15
 repo repo init -u git://github.com/LineageOS/android.git -b lineage-15.1
 cd .repo
 mkdir local_manifests
-cd ../..
+cd ..
 cp -r /home/hkpdev008/Porject-L/SP.xml /home/hkpdev008/line15/.repo/local_manifests
 repo sync --force-sync --no-clone-bundle --no-tags -c
